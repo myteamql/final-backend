955972e4cecb245e50fe787b43f7ca6c08b15ebe
Alex Tschopp <altschop@calpoly.edu>
2019-05-19 14:34:25 -0700

    added autogen creditcard

--- src/main/java/com/example/myteamql/github/finalservlet/entities/CreditCard.java
+++ src/main/java/com/example/myteamql/github/finalservlet/entities/CreditCard.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -12,6 +14,7 @@ import javax.persistence.Id;
 public class CreditCard {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long crNumber;
 
 }
